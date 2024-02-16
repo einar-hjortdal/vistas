82eac5d9ca1a8f5baf6bf6ea0d583fe617625815
Coachonko <info@coachonko.com>
2024-02-16 09:24:16 +0100

    Fix bug with query parameter

--- src/handle_file_create.v
+++ src/handle_file_create.v
@@ -10,11 +10,10 @@ import x.vweb
 // The body of the request should contain the file.
 // Query `gzip`: when present or 'true', the file will be compressed.
 // Requires authorization. (TODO)
-// TODO create directory when provided in file_name
 fn handle_create_file(app &App, mut ctx Context, file_name string) vweb.Result {
 	fn_name := 'handle_create_file'
 
-	should_gzip := ctx.form['gzip'] == '' || ctx.form['gzip'] == 'true'
+	should_gzip := 'gzip' in ctx.query || ctx.query['gzip'] == 'true'
 
 	file_path := '${app.public_directory}/${file_name}'
 	mut file := os.create(file_path) or { return ctx.send_error(err, fn_name) }
@@ -25,8 +24,7 @@ fn handle_create_file(app &App, mut ctx Context, file_name string) vweb.Result {
 	file.close()
 
 	if should_gzip && could_gzip(file_name) {
-		gzip_file_name := '${file_name}${gzip_extension}'
-		gzip_file_path := '${app.public_directory}/${gzip_file_name}'
+		gzip_file_path := '${file_path}/${gzip_extension}'
 		gzip_file_data := gzip.compress(ctx.req.data.bytes()) or {
 			os.rm(file_path) or { return ctx.send_error(err, fn_name) }
 			return ctx.send_error(err, fn_name)
